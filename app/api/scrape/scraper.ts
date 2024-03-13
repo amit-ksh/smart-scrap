a430c0c3b53345ae3f8ad9f28737eb6023bb0799
amit-ksh <ksamit1110@gmail.com>
2024-03-13 13:16:10 +0530

    feat: switch from puppeteer to cheerio

--- app/api/scrape/scraper.ts
+++ app/api/scrape/scraper.ts
@@ -1,24 +1,13 @@
-import puppeteer from "puppeteer";
+import axios from "axios";
+import * as cheerio from "cheerio";
 
-export async function scraper(URL: string, selector: string) {
+export async function scraper(url: string, selector: string) {
   selector = selector ?? "html";
 
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(URL);
+  const html = await axios.get(url);
+  const $ = cheerio.load(html.data);
 
-  const data = await page.evaluate((selector) => {
-    const results: any[] = [];
+  const HTMLContent = $(selector).html()?.replace(/\n|\t/g, "") || "";
 
-    const items = document.querySelectorAll(selector);
-    items.forEach((item) => {
-      results.push(item.innerHTML);
-    });
-
-    return results.join(" ").replace(/\n|\t/g, "");
-  }, selector);
-
-  await browser.close();
-
-  return data;
+  return HTMLContent;
 }
